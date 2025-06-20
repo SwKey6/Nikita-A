3340b64950ae71e6c913978f437186cff72023da
SwKey6 <derevankogleb4@gmail.com>
2025-06-20 18:09:18 +0300

    35

--- shop/Main/views.py
+++ shop/Main/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def Main(request):
+    return render(request, 'Main/index.html')
+
+def About(request):
+    return render(request, 'Main/about.html')
+
+def Contacts(request):
+    return render(request, 'Main/contacts.html')
+
+def Services(request):
+    return render(request, 'Main/services.html')
+
+def Staff(request):
+    return render(request, 'Main/staff.html')
+
+def Portfolio(request):
+    return render(request, 'Main/portfolio.html')
--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -1,22 +1,22 @@
-"""
-URL configuration for shop project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from Main import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+Main = views.Main
+About = views.About
+Contacts = views.Contacts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Main, name='Main'),
+    path('about/', About, name='About'),
+    path('contact/', Contacts, name='Contacts'),
+    path('Services/', views.Services, name='Services'),
+    path('Portfolio/', views.Portfolio, name='Portfolio'),
+    path('Staff/', views.Staff, name='Staff'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
